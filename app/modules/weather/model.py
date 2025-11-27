cca0e482b737d4261b9b683e10324e184617b4f8
USAMA-R-Y <urehman938@gmail.com>
2025-11-27 18:32:27 +0500

    feat: Add base SQLAlchemy model, and implement Country and City SQLAlchemy models and Pydantic schemas.

--- app/modules/weather/model.py
+++ app/modules/weather/model.py
@@ -1,1 +1,20 @@
-# Weather data models
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from app.utils.models import BaseModel
+
+class Country(BaseModel):
+    __tablename__ = "countries"
+
+    name = Column(String, nullable=False)
+    url = Column(String, nullable=True)
+
+    cities = relationship("City", back_populates="country", cascade="all, delete-orphan")
+
+class City(BaseModel):
+    __tablename__ = "cities"
+
+    name = Column(String, nullable=False)
+    url = Column(String, nullable=True)
+    country_id = Column(String, ForeignKey("countries.id"), nullable=False)
+
+    country = relationship("Country", back_populates="cities")
